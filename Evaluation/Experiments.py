434c5b9066e51127d9ab4f6576644d58690e6c3f
Rocket Racoon <54293501+AnnonymousRacoon@users.noreply.github.com>
2022-02-18 23:45:21 +0000

    added experiment class

--- Evaluation/Experiments.py
+++ Evaluation/Experiments.py
@@ -1,40 +1,105 @@
-from DiffusionProject.Algorithms.Coins import HadamardCoin, GroverCoin
-from DiffusionProject.Algorithms.Walks import Boundary, QuantumWalk1D, QuantumWalk2D, OneWayBoundary
-from DiffusionProject.Evaluation.Plotter import plot_distribution2D
+from DiffusionProject.Algorithms.Coins import HadamardCoin
+from DiffusionProject.Algorithms.Walks import Boundary,QuantumWalk, QuantumWalk1D, QuantumWalk2D, QuantumWalk3D, OneWayBoundary
+from DiffusionProject.Evaluation.Plotter import plot_distribution2D, plot_distribution3D
 import pandas as pd
 import subprocess
 
 
 class Experiment:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self,n_dims,n_qubits,shots,max_iterations,stepsize, coin_class = HadamardCoin) -> None:
+        self.n_dims
+        self.n_qubits = n_qubits
+        self.shots = shots
+        self.max_iterations = max_iterations
+        self.stepsize = stepsize
+
+        self.middle_bitstring = "0"+"1"*(n_qubits-1)
+        self.initial_states = [self.middle_bitstring]*n_dims
+        self.system_dims = [n_qubits]*n_dims
+
+        # add edge boundaries
+        self.boundaries = []
+        for dimension in range(n_dims):
+            for bin_value in ["0","1"]:
+                bitstring = bin_value*n_qubits
+                self.boundaries.append(Boundary(bitstring,dimension=dimension))
+
+        self.walk = QuantumWalk(self.system_dims,self.initial_states,coin_class=coin_class,boundaries=self.boundaries)
+        self.walk.step()
+
+    def _build_filetree(self):
+        subprocess.run("mkdir images", shell=True)
+        subprocess.run("mkdir data", shell=True)
+
+    def _plot_distribution(self, experiment_number, results, plot_path):
+        if self.n_dims == 2:
+            title = "diffusion on an {0}x{0} grid with a {1} after {2} steps".format(2**self.n_qubits, self.walk.shift_coin._name,experiment_number)
+            plot_distribution2D(results=results,n_qubits=self.n_qubits,savepath=plot_path,title=title)
+        elif self.n_dims == 3:
+            title = "diffusion on an {0}x{0}*{0} grid with a {1} after {2} steps".format(2**self.n_qubits, self.walk.shift_coin._name,experiment_number)
+            plot_distribution3D(results=results,n_qubits=self.n_qubits,savepath=plot_path,title=title)
 
 
+    def _run_experiment(self):
+        """runs a monte carlo simulation after a varied number of timesteps specified by `self.stepsize` and `self.max_iterations`"""
+        # begin experiment
+        for experiment_number in range(1,self.max_iterations+1,self.stepsize):
 
-# build_filetree
-subprocess.run("mkdir images", shell=True)
-subprocess.run("mkdir data", shell=True)
+            experiment_name = "{}D_Walk_{}_bit_iteration_{}_{}".format(self.n_dims,2**self.n_qubits,experiment_number, self.walk.shift_coin._name)
+            data_path = "data/{}_results.csv".format(experiment_name)
+            plot_path = "images/{}.png".format(experiment_name)
+            
+            results = self.walk.get_results(shots=self.shots)
+            print("results after {} timesteps".format(experiment_number))
 
-# experiment settings
-stepsize = 10
-max_iterations = 100
-n_qubits = 7
-n_dims = 2
-shots = 2048
+            # save and plot results
+            results = pd.DataFrame(results)
+            results.to_csv(data_path)
+            self._plot_distribution(experiment_number=experiment_number, results=results, plot_path=plot_path)
 
-# initial states
-middle_bitstring = "0"+"1"*(n_qubits-1)
-initial_states = [middle_bitstring]*n_dims
-system_dims = [n_qubits]*n_dims
+            # get covariance
+            covariance_matrix = self.walk.get_covariance_tensor()
+            print(covariance_matrix)
+            self.walk.add_n_steps(self.stepsize)
 
-# apply hard boundary at edges
-boundaries = []
-for dimension in range(n_dims):
-    for bin_value in ["0","1"]:
-        bitstring = bin_value*n_qubits
-        boundaries.append(Boundary(bitstring,dimension=dimension))
+    def run(self):
+        """runs a monte carlo simulation after a varied number of timesteps specified by `self.stepsize` and `self.max_iterations`"""
+        self._build_filetree()
+        self._run_experiment()
 
-# initialise walk
-walk = QuantumWalk2D(system_dims,initial_states,coin_class=HadamardCoin,boundaries=boundaries)
-walk.step()
+class Experiment2D(Experiment):
+
+    def __init__(self, n_qubits, shots, max_iterations, stepsize, coin_class = HadamardCoin) -> None:
+        super().__init__(2, n_qubits, shots, max_iterations, stepsize, coin_class)
+
+        # initialise walk
+        self.walk = QuantumWalk2D(self.system_dims,self.initial_states,coin_class=coin_class,boundaries=self.boundaries)
+        self.walk.step()
+
+    def _plot_distribution(self, experiment_number, results, plot_path):
+        title = "diffusion on an {0}x{0} grid with a {1} after {2} steps".format(2**self.n_qubits, self.walk.shift_coin._name,experiment_number)
+        plot_distribution2D(results=results,n_qubits=self.n_qubits,savepath=plot_path,title=title)
+
+
+    def _run_experiment(self):
+        print("Experimenting in {} dimensions with on a {}*{} closed grid and a {}".format(self.n_dims,2**self.n_qubits,2**self.n_qubits, self.walk.shift_coin._name))
+        return super().run_experiment()
+
+class Experiment3D(Experiment):
+    
+    def __init__(self, n_qubits, shots, max_iterations, stepsize, coin_class = HadamardCoin) -> None:
+        super().__init__(3, n_qubits, shots, max_iterations, stepsize, coin_class)
+
+        # initialise walk
+        self.walk = QuantumWalk3D(self.system_dims,self.initial_states,coin_class=coin_class,boundaries=self.boundaries)
+        self.walk.step()
+
+    def _plot_distribution(self, experiment_number, results, plot_path):
+        title = "diffusion on an {0}x{0}*{0} grid with a {1} after {2} steps".format(2**self.n_qubits, self.walk.shift_coin._name,experiment_number)
+        plot_distribution3D(results=results,n_qubits=self.n_qubits,savepath=plot_path,title=title)
+
+
+    def _run_experiment(self):
+        print("Experimenting in {0} dimensions with on a {1}*{1}*{1} closed grid and a {2}".format(self.n_dims,2**self.n_qubits, self.walk.shift_coin._name))
+        return super().run_experiment()
--- Evaluation/Plotter.py
+++ Evaluation/Plotter.py
@@ -1,19 +1,43 @@
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import seaborn as sns
 sns.set_style("whitegrid")
 
 def plot_distribution2D(results,n_qubits,savepath,title = None):
     """plots diffusion for 2D data"""
-    
+
     x,y,alpha = results["dimension_0"],results["dimension_1"],results["probability_density"]
     axes_limit = (2**n_qubits)-1
     if title is None:
-        title = "diffusion on an {}x{} grid".format(axes_limit+1,axes_limit+1)
+        title = "diffusion on an {0}x{0} grid".format(axes_limit+1)
     plt.scatter(x,y,alpha=[i**(1/4) for i in alpha],linewidths=[20*i**0.5 for i in alpha], s = [400*i**0.5 for i in alpha])
     plt.xlim(0,axes_limit)
     plt.ylim(0,axes_limit)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title(title)
+    plt.savefig(savepath,dpi = 300)
+    plt.cla()
+
+
+def plot_distribution3D(results,n_qubits,savepath,title = None):
+    """plots diffusion for 3D data"""
+    
+    x,y,z,alpha = results["dimension_0"],results["dimension_1"],results["dimension_2"],results["probability_density"]
+    axes_limit = (2**n_qubits)-1
+    if title is None:
+        title = "diffusion on an {0}x{0}x{0} grid".format(axes_limit+1)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z,alpha=[i**(1/4) for i in alpha], s = [400*i**0.5 for i in alpha])
+    ax.set_xlim(0, axes_limit)
+    ax.set_ylim(0, axes_limit)
+    ax.set_zlim(0, axes_limit)
+    plt.xlim(0,axes_limit)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title(title)
     plt.savefig(savepath,dpi = 300)
     plt.cla()
