a7574817163378f1396a2a58b583f3fbbbd43e6e
54293501+AnnonymousRacoon@users.noreply.github.com <db3115@ic.ac.uk>
2022-02-20 22:40:08 +0000

    add plot compatibility for python < 3.7

--- Evaluation/Plotter.py
+++ Evaluation/Plotter.py
@@ -1,16 +1,23 @@
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import seaborn as sns
+import sys
+
 sns.set_style("whitegrid")
 
 def plot_distribution2D(results,n_qubits,savepath,title = None):
     """plots diffusion for 2D data"""
 
-    x,y,alpha = results["dimension_0"],results["dimension_1"],results["probability_density"]
+    x,y,probability_density = results["dimension_0"],results["dimension_1"],results["probability_density"]
     axes_limit = (2**n_qubits)-1
     if title is None:
         title = "diffusion on an {0}x{0} grid".format(axes_limit+1)
-    plt.scatter(x,y,alpha=[i**(1/4) for i in alpha],linewidths=[20*i**0.5 for i in alpha], s = [400*i**0.5 for i in alpha])
+
+    if sys.version_info >= (3, 7):
+        alpha = [i**(1/4) for i in probability_density]
+    else:
+        alpha = None
+    plt.scatter(x,y,alpha=alpha,linewidths=[20*i**0.5 for i in alpha], s = [400*i**0.5 for i in probability_density])
     plt.xlim(0,axes_limit)
     plt.ylim(0,axes_limit)
     plt.xlabel('X')
@@ -23,14 +30,19 @@ def plot_distribution2D(results,n_qubits,savepath,title = None):
 def plot_distribution3D(results,n_qubits,savepath,title = None):
     """plots diffusion for 3D data"""
     
-    x,y,z,alpha = results["dimension_0"],results["dimension_1"],results["dimension_2"],results["probability_density"]
+    x,y,z,probability_density = results["dimension_0"],results["dimension_1"],results["dimension_2"],results["probability_density"]
     axes_limit = (2**n_qubits)-1
     if title is None:
         title = "diffusion on an {0}x{0}x{0} grid".format(axes_limit+1)
     
+    if sys.version_info >= (3, 7):
+        alpha = [i**(1/4) for i in probability_density]
+    else:
+        alpha = None
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, z,alpha=[i**(1/4) for i in alpha], s = [400*i**0.5 for i in alpha])
+    ax.scatter(x, y, z,alpha=alpha, s = [400*i**0.5 for i in probability_density])
     ax.set_xlim(0, axes_limit)
     ax.set_ylim(0, axes_limit)
     ax.set_zlim(0, axes_limit)
